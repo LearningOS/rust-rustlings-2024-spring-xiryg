28cd67ea1ee24575faa0ebbbc61d010bbd685845
xiryg <xry102410@gmail.com>
2024-04-09 18:09:25 +0800

    update

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -5,15 +5,20 @@
 //
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub fn foo_if_fizz(fizzish: &str) -> &str {
+
+pub fn foo_if_fizz(fizzish: &str) -> & str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
+    } else if fizzish == "literally anything" {
+        "baz"
     } else {
-        1
+        "unknown"
     }
 }
+
 
 // No test changes needed!
 #[cfg(test)]
